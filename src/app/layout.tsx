76d21836afd992663c0236a53ac7f587bba5ccf6
김용훈 <1224kimyh@t-win.kr>
2026-01-20 16:47:37 +0900

    feat: 파비콘추가 메타 데이터 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,19 @@
+import type { Metadata } from 'next';
 
+export const metadata: Metadata = {
+	icons: {
+		icon: [
+			{ url: '/android-icon.png', sizes: '192x192', type: 'image/png' },
+			{ url: '/ms-icon.png', sizes: '144x144', type: 'image/png' }
+		],
+		apple: [{ url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }]
+	}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="ko">
-			<body>
-				<>{children}</>
-			</body>
+			<body>{children}</body>
 		</html>
 	);
 }
